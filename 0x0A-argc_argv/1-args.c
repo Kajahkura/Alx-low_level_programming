78f2c8c908178d35e11aa8f9862fc529c12a0775
Kajahkura <obedinya4@gmail.com>
2023-03-28 05:36:52 -0700

    arguement vector

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed to it
+ *
+ * Return: 0
+ */
+int main(int agrc, char *agrv[])
+{
+	argc;
+	*argv;
+	printf("0%d\n", argc);
+	return (0);
+}
